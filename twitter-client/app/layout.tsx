9ed2c85f367e12b9ebbf1921fa3b480073d2f8ff
vismay211me359 <vismayms10@gmail.com>
2024-09-24 02:22:11 +0530

    responsive feed-card created

--- twitter-client/app/layout.tsx
+++ twitter-client/app/layout.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import SmallSideBar from "@/components/SmallSideBar";
 import MediumSideBar from "@/components/MediumSideBar";
 import LargeSideBar from "@/components/LargeSideBar";
+import FeedCard from "@/components/FeedCard";
 
 
 export default function RootLayout({ feed, suggestions }: { feed: React.ReactNode, suggestions: React.ReactNode }) {
@@ -44,12 +45,13 @@ export default function RootLayout({ feed, suggestions }: { feed: React.ReactNod
             <div>lorem5000</div></div>}
           {viewPort==="medium"  && <div className="grid grid-cols-[1fr_5fr_4fr] min-h-screen">
               <div><MediumSideBar/></div>
-              <div>lorem5000</div>
+              <div></div>
               <div>lawda</div>
             </div>}
             {viewPort==="large"  && <div className="grid grid-cols-[3fr_4fr_3fr] min-h-screen">
               <div className="justify-self-center"><LargeSideBar/></div>
-              <div>lorem5000</div>
+              <div><FeedCard authorName="vismay" authorHandle="imvismay" tweetTime="15h" tweetContent="Just finished building an awesome project using #ReactJS and #TailwindCSS! 🚀 #webdevelopment.
+              Our new product is finally here! Visit our website to learn more: https://example.com 🛒 #ecommerce #newrelease" media="*/production/tweet_demo_photo.jpeg" tweetTags={["vismau","lawda","testing"]} likesCount={100} commentCount={200} retweetCount={500} trendingCount={50}/></div>
               <div>lawda</div>
               </div>}
         </main>
--- twitter-client/components/FeedCard.tsx
+++ twitter-client/components/FeedCard.tsx
@@ -1,8 +1,103 @@
+import { FaComment, FaRetweet, FaHeart, FaBookmark, FaEllipsisH, FaUpload } from 'react-icons/fa';
+import { IoIosTrendingUp } from "react-icons/io";
+import Image from 'next/image';
 
-const FeedCard = () => {
+interface TweetCardProps {
+  authorName: string;
+  authorHandle: string;
+  tweetTime: string;
+  tweetContent: string;
+  media?: string;
+  tweetTags?: string[];
+  likesCount: number;
+  commentCount: number;
+  retweetCount: number;
+  trendingCount: number;
+}
+
+const FeedCard: React.FC<TweetCardProps> = ({
+  authorName,
+  authorHandle,
+  tweetTime,
+  tweetContent,
+  media,
+  tweetTags = [],
+  likesCount,
+  commentCount,
+  retweetCount,
+  trendingCount
+}) => {
   return (
-    <div>
-      
+    <div className="w-full bg-app-background text-app-text-primary border-x-2 border-app-border border-b-2 pb-2">
+      <div className="grid grid-cols-[1fr_11fr]">
+        <div className='m-2'>
+          <Image
+            src="/images/profile_photo.webp"
+            alt={authorName}
+            width={30}
+            height={30}
+            className="w-full h-auto rounded-full object-cover"
+          />
+        </div>
+        <div>
+          <div className='mt-2 flex justify-between'>
+            <div className="flex items-center space-x-2">
+              <span className="text-app-text-primary font-bold">{authorName}</span>
+              <span className="text-app-text-secondary">@{authorHandle}</span>
+              <span className="text-app-text-secondary">&middot; {tweetTime}</span>
+            </div>
+
+            <div className="text-app-text-secondary hover:text-app-text-primary cursor-pointer mx-3">
+              <FaEllipsisH />
+            </div>
+          </div>
+          <div className='text-app-text-primary mt-2 mr-3'>
+            {tweetContent}
+          </div>
+          {tweetTags.length > 0 && <div className="mt-1 text-app-hashtag">
+            {tweetTags.map((tag, index) => (
+              <span key={index} className="mr-2">
+                #{tag}
+              </span>
+            ))}
+          </div>}
+          {media && (<div className='mt-3 mr-3'>
+            <Image
+              src="/production/tweet_demo_photo.jpeg"
+              alt={authorName}
+              width={40}
+              height={40}
+              className="w-full h-auto object-cover rounded-2xl"
+            />
+          </div>)}
+          <div className='flex justify-between mt-2 text-app-text-primary mr-3'>
+            <div className="flex items-center space-x-2 hover:text-app-icon-hover cursor-pointer">
+              <FaComment />
+              <span>{commentCount}</span>
+            </div>
+            <div className="flex items-center space-x-2 hover:text-app-icon-hover cursor-pointer">
+              <FaRetweet />
+              <span>{retweetCount}</span>
+            </div>
+            <div className="flex items-center space-x-2 hover:text-app-icon-hover cursor-pointer">
+              <FaHeart />
+              <span>{likesCount}</span>
+            </div>
+            <div className="flex items-center space-x-2 hover:text-app-icon-hover cursor-pointer">
+              <IoIosTrendingUp />
+              <span>{trendingCount}</span>
+            </div>
+            <div className='flex align-middle'>
+              <div className="flex items-center space-x-2 hover:text-app-icon-hover cursor-pointer mr-2">
+                <FaBookmark />
+              </div>
+              <div className="flex items-center space-x-2 hover:text-app-icon-hover cursor-pointer">
+                <FaUpload />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
